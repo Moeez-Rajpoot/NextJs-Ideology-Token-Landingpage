c7aa9476a44f8ba454e8ff36a910ae15fa9c859c
Moeez Rajpoot <moeezrajput10@gmail.com>
2024-08-01 11:22:33 +0500

    Section5 Updated

--- app/page.jsx
+++ app/page.jsx
@@ -3,16 +3,18 @@ import Section1bar from "@/components/Section1bar";
 import Section2 from "@/components/Section2";
 import Section3 from "@/components/Section3/section3";
 import Section4 from "@/components/section4/Section4";
+import Section5 from "@/components/section5/Section5";
 
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <Section1 />
+      {/* <Section1 />
       <Section1bar />
       <Section2 />
       <Section3 />
-      <Section4 />
+      <Section4 /> */}
+      <Section5 />
 
 
       </main>
