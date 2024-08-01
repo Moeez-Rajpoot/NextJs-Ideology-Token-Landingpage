c651a623f6202505805d750709dae1fc37ee13fa
Moeez Rajpoot <moeezrajput10@gmail.com>
2024-08-01 12:42:43 +0500

    Updated

--- app/page.jsx
+++ app/page.jsx
@@ -10,12 +10,12 @@ import Section6 from "@/components/section6/Section6";
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <Section1 />
+      {/* <Section1 />
       <Section1bar />
       <Section2 />
       <Section3 />
       <Section4 />
-      <Section5 />
+      <Section5 /> */}
       <Section6 />
 
 
--- components/section6/Section6.jsx
+++ components/section6/Section6.jsx
@@ -9,7 +9,7 @@ import done from "../../public/assets/done.svg";
 export default function Section6() {
   return (
     <div className="h-fit lg:h-screen w-full bg-[#01091c] relative">
-      <div className="w-screen h-fit pt-14 flex flex-col items-center">
+      <div className="w-screen h-fit pt-5 lg:pt-14 flex flex-col items-center">
         <p
           className={`w-full text-[#829fe1] text-md sm:text-xl text-center ${lato.className}`}
         >
@@ -26,20 +26,20 @@ export default function Section6() {
         <Image src={ball} alt="Vector ball" className="h-full w-full" />
       </div>
 
-      <div className="grid grid-cols-2 filter-none  w-[80%] mx-auto h-[57%] z-10 relative">
+      <div className="grid grid-cols-1 text-center lg:text-left lg:grid-cols-2 filter-none  w-[80%] mx-auto h-[57%] z-10 relative">
         {/* Card 1 */}
         <div className="flex mt-14 ">
           <div id="image">
             <Image src={done} alt="Done" className="h-10 w-16"></Image>
           </div>
-          <div id="section6content" className="ml-2">
+          <div id="section6content" className="lg:ml-2">
             <h1
-              className={`${spaceGrotesk.className} text-[24px] -mt-2 font-semibold text-[#7F9ED0]`}
+              className={`${spaceGrotesk.className} -ml-5 lg:-ml-0  text-[24px] -mt-2 font-semibold text-[#7F9ED0]`}
             >
               Layer-2 Chain Integrations
             </h1>
             <p
-              className={`${poppins.className} text-sm mt-2 pr-10 text-[#7F9ED0]`}
+              className={`${poppins.className} text-sm mt-2 ml-4 lg:ml-0 pr-10 text-[#7F9ED0]`}
             >
               Provides enhanced speed and efficiency with layer-2 solutions for
               scalable and cost-effective blockchain operations
@@ -52,14 +52,14 @@ export default function Section6() {
           <div id="image">
             <Image src={done} alt="Done" className="h-10 w-16"></Image>
           </div>
-          <div id="section6content" className="ml-2">
+          <div id="section6content" className="lg:ml-2">
             <h1
-              className={`${spaceGrotesk.className} text-[24px] -mt-2 font-semibold text-[#7F9ED0]`}
+              className={`${spaceGrotesk.className} text-[24px] -ml-5 lg:-ml-0 -mt-2 font-semibold text-[#7F9ED0]`}
             >
               Cross-Chain Compatibility
             </h1>
             <p
-              className={`${poppins.className} text-sm mt-2 pr-10 text-[#7F9ED0]`}
+              className={`${poppins.className} text-sm mt-2 ml-3 lg:ml-0 pr-10 text-[#7F9ED0]`}
             >
               Enables seamless interoperability between different blockchain
               networks, allowing smooth transactions across chains.
@@ -68,18 +68,18 @@ export default function Section6() {
         </div>
 
         {/* Card 3 */}
-        <div className="flex  ">
+        <div className="flex mt-10 lg:mt-0 ">
           <div id="image">
             <Image src={done} alt="Done" className="h-10 w-16"></Image>
           </div>
-          <div id="section6content" className="ml-1">
+          <div id="section6content" className="lg:ml-1">
             <h1
-              className={`${spaceGrotesk.className} text-[24px] -mt-2 font-semibold text-[#7F9ED0]`}
+              className={`${spaceGrotesk.className} text-[24px] -ml-5 lg:-ml-0 -mt-2 font-semibold text-[#7F9ED0]`}
             >
               DeFi Access
             </h1>
             <p
-              className={`${poppins.className} text-sm mt-2 pr-10 text-[#7F9ED0]`}
+              className={`${poppins.className} text-sm ml-3 lg:ml-0 mt-2 pr-10 text-[#7F9ED0]`}
             >
               Offers users opportunities in decentralized finance, such as
               lending and borrowing, directly on the network.
@@ -88,18 +88,18 @@ export default function Section6() {
         </div>
 
         {/* Card 4 */}
-        <div className="flex  ">
+        <div className="flex mt-10  lg:mt-0">
           <div id="image">
             <Image src={done} alt="Done" className="h-10 w-16"></Image>
           </div>
           <div id="section6content" className="ml-0">
             <h1
-              className={`${spaceGrotesk.className} text-[24px] -mt-2 font-semibold text-[#7F9ED0]`}
+              className={`${spaceGrotesk.className} text-[24px] -ml-5 lg:-ml-0 -mt-2 font-semibold text-[#7F9ED0]`}
             >
               Stablecoin Availability
             </h1>
             <p
-              className={`${poppins.className} text-sm mt-2 pr-10 text-[#7F9ED0]`}
+              className={`${poppins.className} text-sm ml-53 lg:ml-0 mt-2 pr-10 text-[#7F9ED0]`}
             >
               Supports stablecoins for transactions, offering a reliable and
               stable form of digital currency.
@@ -108,18 +108,18 @@ export default function Section6() {
         </div>
 
         {/* Card 5 */}
-        <div className="flex  ">
+        <div className="flex mt-10 lg:mt-0 ">
           <div id="image">
             <Image src={done} alt="Done" className="h-10 w-16"></Image>
           </div>
           <div id="section6content" className="ml-0">
             <h1
-              className={`${spaceGrotesk.className} text-[24px] -mt-2 font-semibold text-[#7F9ED0]`}
+              className={`${spaceGrotesk.className} text-[24px] -ml-5 lg:-ml-0 -mt-2 font-semibold text-[#7F9ED0]`}
             >
               Application Rewards and Redemption
             </h1>
             <p
-              className={`${poppins.className} text-sm mt-2 pr-10 text-[#7F9ED0]`}
+              className={`${poppins.className} text-sm ml-3 lg:ml-0 mt-2 pr-10 text-[#7F9ED0]`}
             >
               Rewards users for participating in the network, creating an
               engaging and incentivized ecosystem.
@@ -128,18 +128,18 @@ export default function Section6() {
         </div>
 
         {/* Card 6 */}
-        <div className="flex ">
+        <div className="flex mt-10 lg:mt-0">
           <div id="image">
             <Image src={done} alt="Done" className="h-10 w-16"></Image>
           </div>
-          <div id="section6content" className="ml-1">
+          <div id="section6content" className="lg:ml-1">
             <h1
-              className={`${spaceGrotesk.className} text-[24px] -mt-2 font-semibold text-[#7F9ED0]`}
+              className={`${spaceGrotesk.className} text-[24px] -ml-5 lg:-ml-0 -mt-2 font-semibold text-[#7F9ED0]`}
             >
               Application Rewards and Redemption
             </h1>
             <p
-              className={`${poppins.className} text-sm mt-2 pr-10 text-[#7F9ED0]`}
+              className={`${poppins.className} text-sm ml-3 lg:ml-0 mt-2 pr-10 text-[#7F9ED0]`}
             >
               Rewards users for participating in the network, creating an
               engaging and incentivized ecosystem.
@@ -149,7 +149,7 @@ export default function Section6() {
       </div>
 
       <button
-        className={`mt-4 text-sm ${poppins.className} flex mx-auto mb-5 lg:mb-0  rounded-full bg-[#0b70bc] hover:bg-gradient-to-r from-[#0C71BC] to-[#0A25B1] text-white px-10 py-4 `}
+        className={`mt-10 lg:mt-4 text-sm ${poppins.className} flex mx-auto mb-5 lg:mb-0  rounded-full bg-[#0b70bc] hover:bg-gradient-to-r from-[#0C71BC] to-[#0A25B1] text-white px-10 py-4 `}
       >
         Explore more
       </button>
