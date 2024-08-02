8e5b8d973390ade16c7ce10b866c119111a54b18
Moeez Rajpoot <moeezrajput10@gmail.com>
2024-08-02 12:03:20 +0500

    Footer Updated

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -10,37 +10,37 @@ const poppins = Poppins({ weight: "500", subsets: ["latin"] });
 const poppinlight = Poppins({ weight: "300", subsets: ["latin"] });
 export default function Footer() {
   return (
-    <div className="h-[85vh] w-full bg-[#000717] flex flex-col  items-center">
-      <div className="w-[60%] flex  mt-28 mr-5 h-72">
+    <div className=" h-fit lg:h-[85vh] w-full bg-[#000717] flex flex-col  items-center">
+      <div className="w-full lg:w-[60%] flex flex-col lg:flex-row items-center lg:items-start  mt-28 lg:mr-5 h-72">
         {/* text div */}
-        <div className="w-1/3 flex justify-center ">
+        <div className="w-full lg:w-1/3 flex justify-center ">
           <Image src={Logo} alt="logo" className="h-12 w-36"></Image>
         </div>
-        <div className="w-1/3  flex justify-center ">
+        <div className="w-full lg:w-1/3 mt-5 lg:mt-0  flex justify-center ">
           <Image src={footer} alt="logo" className="h-12 w-36"></Image>
         </div>
-        <div className="w-1/3  flex flex-col items-center">
-          <h3 className={`mt-2 ${poppins.className} text-[#879cce] mr-10 `}>
+        <div className="w-full lg:w-1/3 mt-3 lg:mt-0  flex flex-col items-center">
+          <h3 className={`mt-2 ${poppins.className} text-[#879cce] lg:mr-10 `}>
             NAVIGATION
           </h3>
 
           <p
-            className={`mt-9 w-fit -ml-[33%] text-sm ${poppinlight.className} text-[#879cce] hover:border-b-[1px] hover:border-[#7a91c5] cursor-default  `}
+            className={`mt-9 w-fit lg:-ml-[33%] text-sm ${poppinlight.className} text-[#879cce] hover:border-b-[1px] hover:border-[#7a91c5] cursor-default  `}
           >
             Docs
           </p>
           <p
-            className={`mt-4 w-fit -ml-[28%] text-sm ${poppinlight.className} text-[#879cce] hover:border-b-[1px] hover:border-[#7a91c5] cursor-default `}
+            className={`mt-4 w-fit lg:-ml-[28%] text-sm ${poppinlight.className} text-[#879cce] hover:border-b-[1px] hover:border-[#7a91c5] cursor-default `}
           >
             Games
           </p>
           <p
-            className={`mt-4 w-fit -ml-[24%] text-sm ${poppinlight.className} text-[#879cce] hover:border-b-[1px] hover:border-[#7a91c5] cursor-default `}
+            className={`mt-4 w-fit lg:-ml-[24%] text-sm ${poppinlight.className} text-[#879cce] hover:border-b-[1px] hover:border-[#7a91c5] cursor-default `}
           >
             Mainlads
           </p>
           <p
-            className={`mt-4 w-fit -ml-[18%] text-sm ${poppinlight.className}  text-[#879cce] hover:border-b-[1px] hover:border-[#7a91c5] cursor-default  `}
+            className={`mt-4 w-fit lg:-ml-[18%] text-sm ${poppinlight.className}  text-[#879cce] hover:border-b-[1px] hover:border-[#7a91c5] cursor-default  `}
           >
             Work as Pro
           </p>
